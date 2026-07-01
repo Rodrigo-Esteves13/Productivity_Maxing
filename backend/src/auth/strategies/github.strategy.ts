de0046b477be1c231edb4c341dc9a285c1a9552a
Rodrigo-Esteves13 <rodrigo.m.esteves06@gmail.com>
2026-07-01 20:24:19 +0100

    linter fix error

--- backend/src/auth/strategies/github.strategy.ts
+++ backend/src/auth/strategies/github.strategy.ts
@@ -9,9 +9,9 @@ import { AuthService } from '../auth.service';
 export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
   constructor(private authService: AuthService) {
     super({
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.GITHUB_CALLBACK_URL,
+      clientID: process.env.GITHUB_CLIENT_ID || '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
+      callbackURL: process.env.GITHUB_CALLBACK_URL || '',
       scope: ['user:email'],
       passReqToCallback: true,
     });
